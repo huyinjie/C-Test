a4e679801600846120d2dcb395007fb9e9bd6773
huyinjie <huyinjie@gmail.com>
2016-12-21 13:48:37 +0800

    debug

--- Experiment And Rank Examination Guide To Programming In C/5.2_2.c
+++ Experiment And Rank Examination Guide To Programming In C/5.2_2.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
+int primer_number(int a)
+{
+    int i;
+    for(i=2;i<2/a;i++)
+    {
+        if (a%i==0) return 1;
+    }
+}
 int main(void)
 {
-    int i,j;
-    printf("prime number are: ");
-    666: for(i=500;i<1001;i++)
+    int i;
+    for(i=500;i<1001;i++)
     {
-        for(j=2;j<=(int)sqrt((double)i);j++)
-            if (i%j==0) goto:666
-        printf("%4d\n",i);
+        if(primer_number(i)==0) printf("%4d\n",i);
     }
-    return 0;
 }
