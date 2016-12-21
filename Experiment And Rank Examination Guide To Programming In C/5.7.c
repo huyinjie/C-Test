3ed5a48390c452d00f510c1c833945fd45a053a8
huyinjie <huyinjie@gmail.com>
2016-12-22 00:01:47 +0800

    add 5.7.c

---
+++ Experiment And Rank Examination Guide To Programming In C/5.7.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+int main(void) 
+{ 
+    int j,i,s; 
+    for(j=1;j<=1000;j++) 
+    { 
+        s=0; 
+        for(i=1;i<j;i++) 
+        {
+            if(j%i==0) s=s+i; 
+        }
+        if(s==j) printf("%d\n",s); 
+    } 
+} 
