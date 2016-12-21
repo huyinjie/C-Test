e793be1cf932fad9fe038e4eeb826c6407852226
huyinjie <huyinjie@gmail.com>
2016-12-21 12:55:08 +0800

    debug

--- Experiment And Rank Examination Guide To Programming In C/5.2_2.c
+++ Experiment And Rank Examination Guide To Programming In C/5.2_2.c
@@ -4,14 +4,12 @@
 int main(void)
 {
     int i,j;
+    printf("prime number are: ");
     for(i=500;i<1001;i++)
     {
-        for(j=2;j<=sqrt(i);j++)
-        {
-            if (i%j!=0) continue;
-            else break;
-        }
-        printf("%d",i);
+        for(j=2;j<=(int)sqrt((double)i);j++)
+            if (i%j==0) break;
+        printf("%4d\n",i);
     }
     return 0;
 }
