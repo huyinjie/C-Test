a2663a8ddfb8401bddcccdec815683d24f82f1d8
huyinjie <huyinjie@gmail.com>
2016-12-21 12:24:17 +0800

    add 5.2_3

---
+++ Experiment And Rank Examination Guide To Programming In C/5.2_3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int max(int a,int b)
+{
+    if (a>b) return a;
+    else return b;
+}
+int min(int a,int b)
+{
+    if (a<b) return a;
+    else return b;
+}
+
+int main(void)
+{
+    int a,b,x,y;
+    scanf("%d%d",&a,&b);
+    x=max(a,b);
+    y=min(a,b);
+
+    if (m!=0)
+    {
+        m=x%y;
+        x=y;
+        y=m;
+    }
+    printf("the largest common divisor is %d\n",x);
+    printf("the least common multiple is %d\n",(a*b)/x);
+
+    return 0;
+}
