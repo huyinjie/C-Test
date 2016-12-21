a5312915154659c457f25f8682883660bc392a7e
huyinjie <huyinjie@gmail.com>
2016-12-21 17:25:15 +0800

    debug

---
+++ Experiment And Rank Examination Guide To Programming In C/5.8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int high(int n)
+{
+    float m;
+    m=100/(pow(2,n));
+    return m;
+}
+
+int main(void)
+{
+    float sum=100,n,i;
+    for(i=0;i<8;i++)
+    {
+        sum=sum+100/(pow(2,n))*2;
+    }
+    printf("sum=%f\n",sum);
+    printf("8th=%f\n",high(8));
+    return 0;
+}
