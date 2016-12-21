7b9e5ca57c4b1391c8dec0f8a1ee326bbfd500ac
huyinjie <huyinjie@gmail.com>
2016-12-21 15:50:46 +0800

    add 5.2_1.c

---
+++ Experiment And Rank Examination Guide To Programming In C/5.2_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int unit(int i)
+{
+    return i%10;
+}
+int decade(int i)
+{
+    return i%100/10;
+}
+int hundreds(int i)
+{
+    return i/100;
+}
+
+int main(void)
+{
+    int i,a,b,c;
+    for(i=100;i<=999;i++)
+    {
+        a=hundreds(i);
+        b=decade(i);
+        c=unit(i);
+        if(i==a*a*a+b*b*b+c*c*c) printf("%3d\n",i); 
+    }
+    return 0;
+}
