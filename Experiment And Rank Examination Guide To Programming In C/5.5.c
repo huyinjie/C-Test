ce856df7b643f395dadb89f38d57474657e21668
huyinjie <huyinjie@gmail.com>
2016-12-21 16:27:28 +0800

    add 5.5.c

---
+++ Experiment And Rank Examination Guide To Programming In C/5.5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int even(int a)
+{
+    if (a%2==0) return 1;
+    else return 0;
+}
+int main(void)
+{
+    int m;
+    float i,sum=0;
+    scanf("%d",&m);
+
+    for(i=1;i<=m;i++)
+    {
+        if(even(i)==1) sum=sum-1/i;
+        if(even(i)==0) sum=sum+1/i;
+    }
+    printf("%.2f\n",sum);
+    return 0;
+}
