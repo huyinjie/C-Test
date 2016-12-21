395518d5046aad7f4eedcc4e5fa247be768ddf9f
huyinjie <huyinjie@gmail.com>
2016-12-21 16:56:08 +0800

    debug

--- Experiment And Rank Examination Guide To Programming In C/5.9.c
+++ Experiment And Rank Examination Guide To Programming In C/5.9.c
@@ -5,7 +5,7 @@ int main(void)
     for(i=0;i<9;i++)
     {
         for(j=0;j<9-i;j++)
-        printf("%d%d%d",(i+1),(j+1),(i+1)*(j+1));
+        printf("%d%d=%2d  ",(i+1),(j+1),(i+1)*(j+1));
     }
     printf("\n");
     return 0;
