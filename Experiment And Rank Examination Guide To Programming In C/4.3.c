040d24fb33fe30f244ff624d66223fb383ca2fcc
huyinjie <huyinjie@gmail.com>
2016-12-23 01:12:00 +0800

    add 4.3

---
+++ Experiment And Rank Examination Guide To Programming In C/4.3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    int x;
+    scanf("%d",&x);
+    switch()
+    {
+        case x<2:printf("%d\n",3*x+5);
+        case x<12&&x>=2:printf("%d\n",x*x);
+        case x>=12:printf("%d\n",4*x-7);
+    }
+    return 0;
+}
