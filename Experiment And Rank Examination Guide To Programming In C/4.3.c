148893d2e7a1cb9e5f4eb19db130e2f58b3108b4
huyinjie <huyinjie@gmail.com>
2016-12-23 01:17:50 +0800

    add 4.3

--- Experiment And Rank Examination Guide To Programming In C/4.3.c
+++ Experiment And Rank Examination Guide To Programming In C/4.3.c
@@ -3,11 +3,10 @@ int main(void)
 {
     int x;
     scanf("%d",&x);
-    switch()
-    {
-        case x<2:printf("%d\n",3*x+5);
-        case x<12&&x>=2:printf("%d\n",x*x);
-        case x>=12:printf("%d\n",4*x-7);
-    }
+    
+    if (x<2) printf("%d\n",3*x+5);
+    if (x<12&&x>=2) printf("%d\n",x*x);
+    if (x>=12) printf("%d\n",4*x-7);
+    
     return 0;
 }
