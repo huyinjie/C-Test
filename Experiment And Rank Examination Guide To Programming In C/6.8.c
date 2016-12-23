ed98d3ed659048699837dc4bea7cc71d7c46d765
huyinjie <huyinjie@gmail.com>
2016-12-23 18:22:09 +0800

    debug

--- Experiment And Rank Examination Guide To Programming In C/6.8.c
+++ Experiment And Rank Examination Guide To Programming In C/6.8.c
@@ -2,12 +2,19 @@
 int main(void)
 {
     int a[3][4];
-    int i,j,max;
+    int i,j,max,m,n;
     for(i=0;i<3;i++)
         for(j=0;j<4;j++)
             scanf("%d",&a[i][j]);
     max=a[0][0];
     for(i=0;i<3;i++)
         for(j=0;j<4;j++)
-            
+            if(max<a[i][j])
+            {
+                max=a[i][j];
+                m=i;
+                n=j;
+            }
+    printf("a[%d][%d]=%d\n",m,n,max);
+    return 0;
 }
