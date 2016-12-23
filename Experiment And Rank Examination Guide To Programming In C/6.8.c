f705f50ac9e36116ada7a28158884e3a089810f3
huyinjie <huyinjie@gmail.com>
2016-12-23 18:14:05 +0800

    add 6.8

---
+++ Experiment And Rank Examination Guide To Programming In C/6.8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    int a[3][4];
+    int i,j,max;
+    for(i=0;i<3;i++)
+        for(j=0;j<4;j++)
+            scanf("%d",&a[i][j]);
+    max=a[0][0];
+    for(i=0;i<3;i++)
+        for(j=0;j<4;j++)
+            
+}
