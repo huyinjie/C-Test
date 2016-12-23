e9e532af7b81cf2585b700a37b4b06a6d330f124
huyinjie <huyinjie@gmail.com>
2016-12-23 19:07:54 +0800

    add 6.9

---
+++ Experiment And Rank Examination Guide To Programming In C/6.9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+    int a[10][11]={0,1};
+    int i,j;
+    for(i=1;i<10;i++)
+        for(j=1;j<=i+1;j++)
+            a[i][j]=a[i-1][j]+a[i-1][j-1];
+    for(i=0;i<10;i++)
+    {
+        for(j=1;j<=i+1;j++)
+        {
+            printf("%3d  ",a[i][j]);
+        }
+    printf("\n");
+    }
+    return 0;
+}
