7056d217f2fcbb3b7a3290d76977b1d448bdc406
huyinjie <huyinjie@gmail.com>
2016-12-23 15:09:36 +0800

    debug

--- Experiment And Rank Examination Guide To Programming In C/6.7.c
+++ Experiment And Rank Examination Guide To Programming In C/6.7.c
@@ -2,15 +2,24 @@
 int main(void)
 {
     int a[5][5];
-    int i,j,max;
+    int i,j,k;
     for(i=0;i<5;i++)
         for(j=0;j<5;j++)
             scanf("%d",&a[i][j]);
         printf("\n");
     
+    for(i=1;i<5;i++)
+        for(j=0;j<i;j++)
+        {
+            k=a[i][j];
+            a[i][j]=a[j][i];
+            a[j][i]=k;
+        }
+    
     for(i=0;i<5;i++)
-        for(j=0;j<i+1;j++)
-            sum=sum+a[i][j];
-    printf("%d\n",sum);
+        for(j=0;j<5;j++)
+           printf("%4d\n",a[i][j]);
+        printf("\n");
+    
     return 0;
 }
