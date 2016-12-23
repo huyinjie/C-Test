9c3d5bf0db7bb4e2c7c06f046116448bd3eb8005
huyinjie <huyinjie@gmail.com>
2016-12-23 14:04:20 +0800

    add 6.3 add 6.5

---
+++ Experiment And Rank Examination Guide To Programming In C/6.5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    int a[5][5];
+    int i,j,sum=0;
+    for(i=0;i<5;i++)
+        for(j=0;j<5;j++)
+            scanf("%d",&a[i][j]);
+    for(i=0;j=0;i<5;i++,j++)
+        sum=sum+a[i][j];
+    printf("%d",sum);
+    return 0;
+}
