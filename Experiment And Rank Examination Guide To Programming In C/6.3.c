97f12d768064c1dc621ccfabd8303998501bd335
huyinjie <huyinjie@gmail.com>
2016-12-23 14:20:55 +0800

    debug

--- Experiment And Rank Examination Guide To Programming In C/6.3.c
+++ Experiment And Rank Examination Guide To Programming In C/6.3.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#define N 5
 int main(void)
 {
-    int a[i];
+    int a[N+1];
+    int i,max;
+    for(i=0;i<N;i++)
+        scanf("%d",&a[i]);
+    scanf("%d",&a[N]);
+    for(i=N-1;i>=0;i--)
+    {
+        if(a[i]>a[i+1])
+        {
+            max=a[i];
+            a[i]=a[i+1];
+            a[i+1]=max;
+        }
+    }
+    return 0;
 }
