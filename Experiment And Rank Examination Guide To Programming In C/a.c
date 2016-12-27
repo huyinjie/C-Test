607b77300e3e88a5b31992db766d3c875b5440ef
huyinjie <huyinjie1998@gmail.com>
2016-12-27 21:24:59 +0800

    add a.c

---
+++ Experiment And Rank Examination Guide To Programming In C/a.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{
+    char str1[20],str2[20];
+    int i=0,k=0,j=0;
+    
+    scanf("%s",str1);
+    scanf("%s",str2);
+    
+    while(str1[i]!=EOF)
+    {
+        i++;
+        k++;
+    }
+    
+    while(str2[j]!=EOF)
+    {
+        str1[k+1]=str2[j];
+        j++;
+        k++;
+    }
+    printf("%s\n",strlink(str1,str2));
+    
+}
