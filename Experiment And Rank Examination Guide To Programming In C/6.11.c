747cfdbc9aa14554f56d1254bdcf4611d66d8486
huyinjie <huyinjie1998@gmail.com>
2016-12-27 21:22:29 +0800

    debug

--- Experiment And Rank Examination Guide To Programming In C/6.11.c
+++ Experiment And Rank Examination Guide To Programming In C/6.11.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 int main(void)
 {
-    void strlink(char str1[],char str2[]);
+    //char strlink(char str1[20],char str2[20]);
     char str1[20],str2[20];
     scanf("%s",str1);
     scanf("%s",str2);
-    printf("%s",strlink(str1[20],str2[20]));
+    //printf("%s\n",strlink(str1,str2));
     return 0;
 }
 
-void strlink(char str1[],char str2[])
+/*
+char strlink(char str1[20],char str2[20])
 {
     int i=0,k=0,j=0;
-    while(str1[i]!='/0')
+    while(str1[i]!=EOF)
     {
         i++;
         k++;
     }
     
     do{
-        str1[k]=str2[j];
+        str1[k+1]=str2[j];
         j++;
         k++;
     }
-    while(str2[j]!='\0');
-    str1[k]='\0';
-    return str1[];
+    while(str2[j]!=EOF);
+    return str1[20];
 }
+*/
