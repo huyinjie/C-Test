7e90e0af89e1955a8343523189082f0cab6abe9b
huyinjie <huyinjie1998@gmail.com>
2016-12-28 17:27:58 +0800

    debug

--- Experiment And Rank Examination Guide To Programming In C/6.9.c
+++ Experiment And Rank Examination Guide To Programming In C/6.9.c
@@ -1,24 +1,26 @@
+//10位同学参加3门课程考试，找出每门课的最高分
 #include <stdio.h>
 int main(void)
 {
-    int a[5],i,j,max;
-    
-    for(i=0;i<5;i++)
-        scanf("%d",&a[i]);
-    printf("\n");
-    
-    for(i=0;i<4;i++)
-        for(j=i+1;j<5;j++)
+    int a[3][10];
+    int i,j,max,sum=0;
+    for(i=0;i<3;i++)
+    {
+        printf("Lesson %d\n",i+1);
+        for(j=0;j<10;j++)
         {
-            if(a[i]>a[j])
-            {
-                max=a[i];
-                a[i]=a[j];
-                a[j]=max;
-            }
+            printf("student %d:  ",j+1);
+            scanf("%d",&a[i][j]);
         }
-    
-    for(i=0;i<5;i++)
-        printf("%d\n",a[i]);
+        printf("\n");
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0,max=a[i][j];j<10;j++)
+        {
+            if(a[i][j]>max) max=a[i][j];
+        }
+        printf("the max of lesson %d is %d\n",i,max);
+    }
     return 0;
 }
