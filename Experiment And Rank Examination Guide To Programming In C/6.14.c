1bbbcbc5eb426a24823d62065f21980108947796
huyinjie <huyinjie1998@gmail.com>
2016-12-28 20:35:45 +0800

    debug

--- Experiment And Rank Examination Guide To Programming In C/6.14.c
+++ Experiment And Rank Examination Guide To Programming In C/6.14.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 int main(void)
 {
-    int i,num[25]={0};
+    int i,m,num[25]={0};
+    int max=num[0];
     char c;
     while((c=getchar())!='#')
     {
         if(c>=65 && c<=90) num[c-'A']+=1;
-        
     }
+    for(i=0;i<26;i++)
+        if(max<num[i])
+        {
+            max=num[i];
+            m=i;
+        } 
+    printf("%s:%d",i+'A',max);
 }
