e45bf2a846f11e492f43d690b4a9ffe78ff2eb12
huyinjie <huyinjie1998@gmail.com>
2016-12-28 17:46:16 +0800

    add 6.13

--- Experiment And Rank Examination Guide To Programming In C/6.12.c
+++ Experiment And Rank Examination Guide To Programming In C/6.12.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
 int main(void)
 {
-    //int my_strlen(const char * str)
-    char str[20];
-    scanf("%s",str);
-    printf("%d",my_strlen(str));
-    return 0;
-}
-
-int my_strlen(const char * str)
-{
-    const char *s;
-    for (s = str; *s; ++s)
-        ;
-    return (s - str);
+    
 }
---
+++ Experiment And Rank Examination Guide To Programming In C/6.13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char str1[20],str2[20],ch;
+    scanf("%s",str1);
+    int i,l=strlen(str1);
+    strcpy(str2,str1);
+    for(i=0;i<l/2;i++)
+    {
+        ch=str1[i];
+        str1[i]=str1[l-i-1];
+        str1[l-i-1]=ch;
+    }
+    if (strcmp(str1,str2)==0) printf("yes\n");
+    else printf("no\n");
+    return 0;
+}
