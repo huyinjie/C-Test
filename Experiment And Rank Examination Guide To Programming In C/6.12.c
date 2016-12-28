688e831c025b5e398fc885db6ef66f337dc42ee8
huyinjie <huyinjie1998@gmail.com>
2016-12-28 18:45:19 +0800

    debug

--- Experiment And Rank Examination Guide To Programming In C/6.12.c
+++ Experiment And Rank Examination Guide To Programming In C/6.12.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 int main(void)
 {
-    
+    char str[20];
+    scanf("%s",str);
+    int l=my_strlen(str);
+    printf("%d",l);
+    return 0;    
+}
+
+size_t
+my_strlen(const char *str)
+{
+    const char *s;
+    for (s = str; *s; ++s)
+                ;
+    return (s - str);
 }
