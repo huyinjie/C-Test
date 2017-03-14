86125c90fb5bf9e782a1a8322e085f1df3a41c0d
Yinjie Hu <huyinjie1998@gmail.com>
2017-03-14 21:35:07 +0800

    编写一个统计空格nb、制表符nt和换行个数nl的程序

---
+++ The C Programming Language 2th/1-8.c
@@ -0,0 +1,14 @@
+//编写一个统计空格nb、制表符nt和换行个数nl的程序
+#include <stdio.h>
+int main(void)
+{
+    int nb=0,nt=0,nl=0;
+    while(c=getchar()!=EOF)
+    {
+        if(c == ' ') ++nb;
+        if(c == '\t') ++nt;
+        if(c == '\n') ++nl;
+    }
+    printf("blanks=%d,tabs=%d,newlines=%d",nb,nt,nl);
+    return 0;
+}
