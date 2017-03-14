4b0441927c7476e453c54831bfc80ea0ccac3a41
Yinjie Hu <huyinjie1998@gmail.com>
2017-03-14 21:42:51 +0800

    验证布尔表达式getchar()!=EOF的取值是0还是1

--- The C Programming Language 2th/1-8.c
+++ The C Programming Language 2th/1-8.c
@@ -2,12 +2,20 @@
 #include <stdio.h>
 int main(void)
 {
-    int c,nb=0,nt=0,nl=0;
+    int c,nb,nt,nl;
+
+    nb = 0;
+    nt = 0;
+    nl = 0;
+    
     while(c=getchar()!=EOF)
     {
-        if(c == ' ') ++nb;
-        if(c == '\t') ++nt;
-        if(c == '\n') ++nl;
+        if(c == ' ') 
+            ++nb;
+        else if(c == '\t') 
+            ++nt;
+        else if(c == '\n') 
+            ++nl;
     }
     printf("blanks=%d,tabs=%d,newlines=%d",nb,nt,nl);
     return 0;
