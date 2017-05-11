0e2d70198c88b4c565a2bf2203211ef4916d5ba7
Yinjie Hu <huyinjie1998@gmail.com>
2017-05-11 22:58:01 +0800

    debug

--- Test/Experiment And Rank Examination Guide To Programming In C/5.7.c
+++ Test/Experiment And Rank Examination Guide To Programming In C/5.7.c
@@ -21,25 +21,24 @@ int main(void)
 #include <stdio.h>
 int main(void)
 {
-	  static int k[10];
-	  int i, j, n, s;
-	  for (j = 2; j <= 1000; j++){
-		    n = -1; s = j;
-		    for (i = 1; i<j; i++){
-			    if (j%i == 0){
+	static int k[10];
+	int i, j, n, s;
+	for (j = 2; j <= 1000; j++) {
+		n = -1; s = j;
+		for (i = 1; i<j; i++) {
+			if (j%i == 0) {
 				n++;
 				s = s - i;
 				k[n] = i;
 			}
 		}
-		if (s == 0)
-		{
+		if (s == 0){
 			printf("%d是一个完数,它的因子是", j);
-			for (i = 0; i<n; i++)
+			for (i = 0; i<n; i++){
 				printf("%d ,", j);
+			}
 			printf("%d\n", k[n]);
 		}
-
 	}
 	return 0;
 }
