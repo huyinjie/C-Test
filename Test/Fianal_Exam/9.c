350c9c77096f96d41cff2a660226c0ff376e56eb
Yinjie Hu <huyinjie1998@gmail.com>
2017-05-25 22:40:34 +0800

    debug

--- Test/Fianal_Exam/9.c
+++ Test/Fianal_Exam/9.c
@@ -1,39 +1,15 @@
 //最大公约数和最小公倍数（辗转相除法）
-//因一个函数两个返回值，故使用指针返回值，return返回状态
-
-#include <stdio.h>
-int max(int a, int b);
-int min(int a, int b);
-int OutPut(int a, int b); //依次返回最大公倍数，最小公约数
-
-int main(void)
+#include<stdio.h>  
+void main()   /*  辗转相除法求最大公约数 */
 {
-	int a, b;
+	int m, n, a, b, t, c;
+	printf("Input two integer numbers:\n");
 	scanf("%d%d", &a, &b);
-	OutPut(a, b);
-	printf("Least common multiple:%d",)
-	return 0;
-}
-int OutPut(int a, int b) {
-	int x, y;
-	x = max(a, b);
-	y = min(a, b);
-
-	while (m != 0)
+	m = a;   n = b;
+	while (b != 0)  /* 余数不为0，继续相除，直到余数为0 */
 	{
-		m = x%y;
-		x = y;
-		y = m;
+		c = a%b; a = b;  b = c;
 	}
-	printf("the largest common divisor is %d\n", x);
-	printf("the least common multiple is %d\n", (a*b) / x);
-
-}
-int max(int a, int b){
-	if (a>b) return a;
-	else return b;
-}
-int min(int a, int b){
-	if (a<b) return a;
-	else return b;
+	printf("The largest common divisor:%d\n", a);
+	printf("The least common multiple:%d\n", m*n / a);
 }
