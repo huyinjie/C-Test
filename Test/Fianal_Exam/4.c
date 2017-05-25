d7c28d363c99211603d0610d0db0c27b819754d3
Yinjie Hu <huyinjie1998@gmail.com>
2017-05-25 12:31:34 +0800

    将三个数从小到大输出（利用指针）

---
+++ Test/Fianal_Exam/4.c
@@ -0,0 +1,26 @@
+/*
+将三个数从小到大输出（利用指针）
+冒泡排序
+*/
+#include <stdio.h>
+int main(void)
+{
+	int a[3],i,j,temp;
+	for (i = 0; i < 3; i++) {
+		scanf("%d", &a[i]);
+	}
+	for(i=0;i<2;i++){
+		for(j=0;j<2-i;j++){
+			if(*(a+j)>*(a+j+1)){
+				temp = *(a + j);
+				*(a + j) = *(a + j + 1);
+				*(a + j + 1) = temp;
+			}
+		}
+	}
+	for (i = 0; i < 3; i++) {
+		printf("%d ", *(a + i));
+	}
+
+	return 0;
+}
