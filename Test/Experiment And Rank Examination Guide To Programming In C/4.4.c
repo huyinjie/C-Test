75faecd24a6974036e9cecb7d6c319f3091594db
Yinjie Hu <huyinjie1998@gmail.com>
2017-06-08 16:18:27 +0800

    debug

--- Test/Experiment And Rank Examination Guide To Programming In C/4.4.c
+++ Test/Experiment And Rank Examination Guide To Programming In C/4.4.c
@@ -1,4 +1,5 @@
 //小写字母转换成大写字母
+//普通版
 #include <stdio.h>
 int main(void)
 {
@@ -6,4 +7,20 @@ int main(void)
     scanf("%c",&m);
     if(m>97&&m<122) printf("%c\n",m-32);
     return 0;
+}
+
+//函数版
+#include <stdio.h>
+char latter(char a);
+int main(void)
+{
+	char m;
+	scanf("%c", &m);
+	 printf("%c\n",latter(m));
+	return 0;
+}
+
+char latter(char m) {
+	if (m >= 97 && m <= 122) return ("%c\n", m - 32);
+	
 }
